73d86678251bb4b4d9895ad6687b60b53a1b57fa
Daniel Pyon <pyondohyun@gmail.com>
2022-11-02 18:08:51 -0700

    selection sort

--- sorts.c
+++ sorts.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void swap(int* x, int* y) {
+    int tmp = *x;
+    *x = *y;
+    *y = tmp;
+}
+
+int* selection_sort(int* list, int n) {
+    for (int i = 0; i < n; i++) {
+        int* min = &list[i];
+        for (int j = i + 1; j < n; j++)
+            if (list[j] < *min)
+                min = &list[j];
+        swap(min, &list[i]);
+    }
+}
+
 int main(int argc, char** argv) {
     if (argc <= 1) {
         fprintf(stderr, "Usage: sorts [...]\n");
@@ -12,6 +28,8 @@ int main(int argc, char** argv) {
 
     for (int i = 0; i < n; i++)
         list[i] = atoi(argv[i + 1]);
+
+    selection_sort(list, n);
 
     for (int i = 0; i < n; i++)
         printf("%d\n", list[i]);
