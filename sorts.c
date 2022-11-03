15b08c22ba3690ff676e7602c60653a3cac1ba96
Daniel Pyon <pyondohyun@gmail.com>
2022-11-02 18:38:01 -0700

    counting sort

--- sorts.c
+++ sorts.c
@@ -7,6 +7,8 @@ void swap(int* x, int* y) {
     *y = tmp;
 }
 
+// Comparison based algorithms
+
 void selection_sort(int* list, int n) {
     for (int i = 0; i < n; i++) {
         int* min = &list[i];
@@ -35,6 +37,35 @@ void insertion_sort(int* list, int n) {
         }
 }
 
+
+
+// TODO: heapsort, bst sort
+
+// Non-comparison algorithms
+
+void counting_sort(int* list, int n, int min, int max) {
+    int range = abs(max - min) + 1;
+    if (min < 0) {
+        // Normalize for negatives
+        for (int i = 0; i < n; i++)
+            list[i] -= min;
+        range = abs(max - min) + 1;
+    }
+    
+    int* freqs = (int*)calloc(range, sizeof(int));
+    for (int i = 0; i < n; i++)
+        freqs[list[i]]++;
+
+    int curr = 0;
+    for (int i = 0; i < range; i++)
+        if (freqs[i] != 0)
+            for (int j = 0; j < freqs[i]; j++)
+                list[curr++] = i;
+
+    for (int i = 0; i < n; i++)
+        list[i] += min;
+}
+
 int main(int argc, char** argv) {
     if (argc <= 1) {
         fprintf(stderr, "Usage: sorts [...]\n");
@@ -47,7 +78,16 @@ int main(int argc, char** argv) {
     for (int i = 0; i < n; i++)
         list[i] = atoi(argv[i + 1]);
 
-    insertion_sort(list, n);
+    int min = list[0];
+    int max = list[0];
+    for (int i = 0; i < n; i++) {
+        if (list[i] < min)
+            min = list[i];
+        if (list[i] > max)
+            max = list[i];
+    }
+
+    counting_sort(list, n, min, max);
 
     for (int i = 0; i < n; i++)
         printf("%d\n", list[i]);
